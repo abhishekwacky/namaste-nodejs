970a339563395aaf1c841db53de91d2b8533104e
Abhishek Kumar <abhishek4ualwys@gmail.com>
2026-02-06 03:12:13 +0530

    thread-pool

--- thread-pool.js
+++ thread-pool.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const crypto = require("crypto");
 
-process.env.UV_THREADPOOL_SIZE = 8;
+process.env.UV_THREADPOOL_SIZE = 2;
 
 crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
   console.log("1 - cryptoPBKDF2 done");
